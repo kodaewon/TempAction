ca7e40c3122d0145842aca29cd66ff1ac316b9d8
kd <kd@sweettracker.co.kr>
2024-12-12 00:16:28 +0900

    aaa

--- src/sample.test.ts
+++ src/sample.test.ts
@@ -2,9 +2,11 @@ import axios from 'axios';
 import * as fs from 'fs';
 import { execSync } from 'child_process';
 
+const execPromise = util.promisify(exec);
+
 async function fetchAndSaveLottoNumber() {
     try {
-        const response = await axios.get('https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=860');
+        const response = await axios.get('https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=859');
         const data = response.data;
         const drwNoDate = data.drwNoDate.replace(/[/\\?%*:|"<>]/g, '-'); // 파일명 안전하게 변경
 
@@ -13,14 +15,14 @@ async function fetchAndSaveLottoNumber() {
         console.log(`File saved as ${fileName}`);
 
         // Git commands to add, commit, and push the file
-        execSync('git config --global user.name "github-actions[bot]"');
-        execSync('git config --global user.email "github-actions[bot]@users.noreply.github.com"');
-        execSync(`git add "${fileName}"`);
-        execSync(`git commit -m "Add lotto number response file for ${drwNoDate}"`);
+        await execPromise('git config --global user.name "github-actions[bot]"');
+        await execPromise('git config --global user.email "github-actions[bot]@users.noreply.github.com"');
+        await execPromise(`git add "${fileName}"`);
+        await execPromise(`git commit -m "Add lotto number response file for ${drwNoDate}"`);
 
         // Git push using GitHub token
         const githubToken = process.env.GITHUB_TOKEN;
-        execSync(`git push https://${githubToken}@github.com/kodaewon/TempAction.git`);
+        await execPromise(`git push https://${githubToken}@github.com/kodaewon/TempAction.git`);
         console.log('File committed and pushed to the repository');
     } catch (error) {
         console.error('Error fetching lotto number:', error);
